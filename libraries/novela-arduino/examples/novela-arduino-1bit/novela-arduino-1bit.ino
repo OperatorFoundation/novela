40cf48c9999321adac26adf9b5a4f58b6fbf8ee3
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2025-07-20 19:55:11 -0500

    Don't upload test sketches when building with cmake directly

--- libraries/novela-arduino/examples/novela-arduino-1bit/novela-arduino-1bit.ino
+++ libraries/novela-arduino/examples/novela-arduino-1bit/novela-arduino-1bit.ino
@@ -1,23 +1,15 @@
 #include <Arduino.h>
 #include <string>
 
-#include <gfx_canvas.h>
 #include <novela.h>
 #include <ReliableConnectionUsbCdc.h>
-#include <ReliableConnectionSerial1.h>
 #include <arduino_clock.h>
 #include <serial_logger.h>
 #include <soft_cursor.h>
-#include <drivers/dvhstx/dvhstx.hpp>
 
-pimoroni::DVHSTX hstx;
-GfxCanvas canvas = GfxCanvas(hstx);
 ReliableConnectionUsbCdc usb = ReliableConnectionUsbCdc();
-ReliableConnectionSerial1* serial1 = ReliableConnectionSerial1::getInstance();
 ArduinoClock ticker = ArduinoClock();
 SerialLogger logger = SerialLogger();
-SoftCursor cursor = SoftCursor(ticker, canvas, logger);
-Novela novela(canvas, serial1, ticker, logger, &cursor);
 
 void setup()
 {
@@ -32,15 +24,7 @@ void setup()
   Serial.println("Hello, Operator.");
   delay(2000); // Wait for USB to be initialized
 
-  serial1->enableXonXoff();
-  serial1->begin();
-
   logger.setLevel(Logger::Level::INFO);
-
-  canvas.begin();
-
-  // Call this last to ensure that everything is initialized before we start up the terminal.
-  novela.begin();
 
   Serial.println("setup() complete.");
 
@@ -49,18 +33,9 @@ void setup()
 
 void loop()
 {
-  std::vector<char> output = serial1->read();
-  if(!output.empty())
-  {
-    novela.process(output);
-    usb.write(output);
-  }
-
-  novela.update();
-
   std::vector<char> input = usb.read();
   if(!input.empty())
   {
-    serial1->write(input);
+    usb.write(input);
   }
 }
